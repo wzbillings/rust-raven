4c3fb2e00d1fbfb56dbcbcd76b55a66f07571a28
wzbillings <39701545+wzbillings@users.noreply.github.com>
2022-03-20 00:35:09 -0400

    chore: removes test.R since all code is done in data cleaning script

--- R/test.R
+++
@@ -1,6 +0,0 @@
-library(jsonlite)
-
-json_file <- here::here("beeradvocate-scraper","beers.json")
-test <- fromJSON(sprintf("[%s]", paste(readLines(json_file), collapse=",")))
-
-test$style <- factor(test$style)
